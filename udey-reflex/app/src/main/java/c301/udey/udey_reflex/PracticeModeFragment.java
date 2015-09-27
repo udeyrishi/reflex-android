a5e39e3603be31f118dc7d7e676a6ecb01e2d3e6
Udey Rishi <udey.rishi@gmail.com>
2015-09-26 19:20:30 -0600

    Added begin practice button template

--- udey-reflex/app/src/main/java/c301/udey/udey_reflex/PracticeModeFragment.java
+++ udey-reflex/app/src/main/java/c301/udey/udey_reflex/PracticeModeFragment.java
@@ -3,8 +3,11 @@ package c301.udey.udey_reflex;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +21,13 @@ public class PracticeModeFragment extends AppModeFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_mode_practice, container, false);
+        Button b = (Button) rootView.findViewById(R.id.begin_practice_button);
+        b.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBegin(v);
+            }
+        });
         return rootView;
     }
 
@@ -25,5 +35,9 @@ public class PracticeModeFragment extends AppModeFragment {
         PracticeModeFragment fragment = new PracticeModeFragment();
         fragment.attachSectionNumber(sectionNumber);
         return fragment;
+    }
+
+    public void onBegin(View view) {
+        Toast.makeText(getActivity(), "Begin pressed.", Toast.LENGTH_SHORT).show();
     }
 }
